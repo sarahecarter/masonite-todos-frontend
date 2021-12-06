0be8f0bc7476b71cb42028823fc420fa9452d20d
sarahecarter <secarter213@gmail.com>
2021-12-06 12:25:45 -0500

    initial commit

---
+++ src/pages/AllPosts.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Post from "../components/Post"
+
+const AllPosts = (props) => {
+    // for each post in the array, render a post component
+    return props.posts.map((post) => {
+        return <Post key={post.id} post={post}/>
+    })
+}
+
+export default AllPosts;
